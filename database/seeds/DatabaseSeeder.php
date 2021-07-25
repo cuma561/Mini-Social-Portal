4da8c16ee2787ab10eec848fed46981895652705
Bartek Krawczak <cuma561@github.com>
2021-07-25 18:28:42 +0200

    Add seed application

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Friend;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+    }
+}
