b43bd013b2c49e59b332138b92ccef95c072e74b
Bartek Krawczak <cuma561@github.com>
2021-07-25 14:21:36 +0200

    Add models files

---
+++ app/Post.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = [
+    	'user_id', 'content',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function comments()
+    {
+        if (is_admin()) {
+            return $this->hasMany('App\Comment')->withTrashed();
+        } else {
+            return $this->hasMany('App\Comment');
+        }
+    }
+
+    public function likes()
+    {
+        return $this->hasMany('App\Like');
+    }
+
+}
